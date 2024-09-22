bf69859e4f0b15a4ed3a31ea224e7715c2c22777
redburden <aburd3@uis.edu>
2024-09-22 14:56:49 -0500

    fix p5 sketch canvas

--- custom_strands/sketch.js
+++ custom_strands/sketch.js
@@ -1,5 +1,7 @@
 function setup() {
   textAlign(CENTER, CENTER);
+  let canvas = createCanvas((n * 2000) / n, n * (2000 / n));
+  canvas.parent("canvas");
   textsize(26);
   text("STRINGS", 100, 100);
 }
