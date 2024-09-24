4c8f5f7c8e0daa10137001345f904d8548f58181
redburden <90938076+redburden@users.noreply.github.com>
2024-09-24 18:33:05 +0000

    blank canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,3 @@
 function setup() {
-  textAlign(CENTER, CENTER);
-  let canvas = createCanvas((n * 2000) / n, n * (2000 / n));
-  canvas.parent("canvas");
-  textsize(26);
-  text("STRINGS", 100, 100);
+  background(200);
 }
