eb784f5ba30c6c9abad76e67d986f9986c646dbe
redburden <90938076+redburden@users.noreply.github.com>
2024-09-24 17:38:36 +0000

    create raw word array for testing phase

--- WordScheduling.js
+++ WordScheduling.js
@@ -1,1 +1,16 @@
+import { generate } from "random-words";
 
+// TO-DO: WordScheduling should be built as a function that takes in rawWords generated from WordAssociation.
+// For now, we will just generate random words for testing.
+
+let rawWords = generate({
+  exactly: 20,
+  maxLength: 10,
+  formatter: (word) => word.toUpperCase(),
+});
+
+//TO-DO: For testing, assign random "weights" to each word in rawWords.
+//    - The weights should be numbers 1-20 used only once.
+//    - Weight will be used as a words' relationship score to the keyword.
+
+console.log(rawWords);
