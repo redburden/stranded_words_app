fadc85cf2064ff33cff9b547371bf3610ab9797f
redburden <90938076+redburden@users.noreply.github.com>
2024-09-24 17:18:35 +0000

    add grid data structures

---
+++ GridCanvas.js
@@ -0,0 +1,28 @@
+let gridCharacter = {
+  xpos: null,
+  ypos: null,
+  letter: "",
+  word: "",
+  top: null,
+  left: null,
+  right: null,
+  bottom: null,
+};
+
+let gridWidth = 6;
+let gridHeight = 8;
+let gridRow = (() => {
+  let row = [];
+  for (let i = 0; i < gridWidth; i++) {
+    row.push(gridCharacter);
+  }
+  return row;
+})();
+
+let grid = (() => {
+  let grid = [];
+  for (let i = 0; i < gridHeight; i++) {
+    grid.push(gridRow);
+  }
+  return grid;
+})();
