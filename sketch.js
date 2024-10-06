9af828da7c5bacd09080792eb05969bcf4fb1cf5
redburden <90938076+redburden@users.noreply.github.com>
2024-10-06 15:39:06 +0000

    create blank canvas

--- sketch.js
+++ sketch.js
@@ -1,3 +1,9 @@
 function setup() {
+  let canvas = createCanvas(600, 800);
+  // canvas.background(200);
   background(200);
+  /* userDimensions = document.getClientRects();
+  canvas.setAttribute("height", userDimensions[0].height); */
 }
+
+function draw() {}
