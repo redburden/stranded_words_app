c46c72d00334af03acee6d662f487c7d3e181397
redburden <90938076+redburden@users.noreply.github.com>
2024-10-06 17:50:50 +0000

    update bubble color on click

--- sketch.js
+++ sketch.js
@@ -16,7 +16,23 @@ function draw() {
 }
 
 // TODO: Use mouseClick to alter bubble at mouse position
-function mouseClicked() {}
+function mouseClicked() {
+  // Get mouseX and mouseY and round them to the nearest 50
+  // Find the bubble at that position
+  // Change the bubble's color
+  let x = mouseX;
+  let y = mouseY;
+  let xRound = Math.round(x / 50) * 50;
+  let yRound = Math.round(y / 50) * 50;
+
+  xRound = Math.floor(xRound / 100);
+  yRound = Math.floor(yRound / 100);
+
+  bubbleNum = yRound * 6 + xRound;
+
+  bubbles[bubbleNum].update();
+  bubbles[bubbleNum].display();
+}
 
 function randChars() {
   // Generate an array of 48 random chars
@@ -33,26 +49,30 @@ function randChars() {
 
 function populateGrid() {
   letters = randChars();
-  let newCircle = (xPos, yPos) => ({
+  gridChars = [];
+
+  let newCircle = (letter, xPos, yPos) => ({
     size: 50,
     xpos: xPos,
     ypos: yPos,
     fillColor: [255],
+    letter: letter,
     display() {
       fill(this.fillColor);
       ellipse(this.xpos, this.ypos, this.size, this.size);
+      fill(0);
+      text(this.letter, this.xpos, this.ypos);
     },
     update() {
-      this.ypos += 1;
+      this.fillColor = [255, 165, 0];
     },
   });
 
-  i = 0;
+  let i = 0;
   for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 6; col++) {
-      bubbles.push(newCircle(50 + col * 100, 50 + row * 100));
+      bubbles.push(newCircle(letters[i], 50 + col * 100, 50 + row * 100));
       bubbles[i].display();
-      bubbles[i].update();
       i++;
     }
   }
